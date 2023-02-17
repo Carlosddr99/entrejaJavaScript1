ad25e838711433b99d25fc79ead52f711321167a
Carlos Daniel <carlosddr1999@hotmail.com>
2023-02-17 17:37:50 +0000

    Añadimos sección donaciones pagadas faltantes

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,13 @@
+
 function abirDialogo(){
     const dialog = document.getElementById('dialogo');
+    const importe = document.getElementById('inputImporte').value;
+    document.getElementById("importeDialog").value = importe;
+    console.log(importe);
     dialog.showModal();
+}
+
+function cerrarDialogo(){
+    const dialog = document.getElementById('dialogo');
+    dialog.close();
 }
